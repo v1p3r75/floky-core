6996b769a140ecc254debf35d8f0db3d37d4b481
v1p3r75 <elfriedv16@gmail.com>
2023-11-23 11:36:16 +0100

    Added some methods to Session facade

--- src/Facades/Session.php
+++ src/Facades/Session.php
@@ -5,4 +5,35 @@ namespace Floky\Facades;
 class Session extends Facades
 {
 
+    public static function get($key): string | false
+    {
+
+        if (isset($_SESSION[$key])) {
+
+            return $_SESSION[$key];
+        }
+
+        return false;
+    }
+
+    public static function set($key, $value): bool
+    {
+
+        $_SESSION[$key] = $value;
+
+        return true;
+    }
+
+    public static function getId(): string | false
+    {
+
+        return session_id();
+    }
+
+    public static function setId(string $value): string | false
+    {
+
+        return session_id($value);
+    }
+    
 }
