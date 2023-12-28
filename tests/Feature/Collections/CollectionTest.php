833c96d104eec2b002805f47f0c477e699d10f69
v1p3r75 <elfriedv16@gmail.com>
2023-12-28 19:50:34 +0100

    Add some tests (Collections, ...)

--- tests/Feature/Collections/CollectionTest.php
+++ tests/Feature/Collections/CollectionTest.php
@@ -1,18 +1,34 @@
 <?php
+
 use Floky\Collections\Collection;
 
-beforeEach(function() {
+beforeEach(function () {
 
     $this->items = ['first', 'second', 'third', 'fourth'];
     $this->collection = new Collection($this->items);
 });
 
-it('should return all items', function() {
+it('should return all items', function () {
 
     expect($this->collection->all())->toMatchArray($this->items);
 });
 
-it('should count items values successfully', function() {
+it('should return all items with callback', function () {
+
+    expect($this->collection->all(
+        function ($a) {
+            return $a . "-updated";
+        }
+    ))->toMatchArray(['first-updated', 'second-updated', 'third-updated', 'fourth-updated']);
+
+});
+
+it('should return a random value', function () {
+
+    expect($this->collection->random())->toBeIn($this->items);
+});
+
+it('should count items values successfully', function () {
 
     expect($this->collection->count())->toBe(4);
 });
