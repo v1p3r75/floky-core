5786cef85ca684de0605182b4cb4d2892829e72c
v1p3r75 <elfriedv16@gmail.com>
2024-05-21 11:56:30 +0100

    Facade memory error fixed

--- src/Facades/Facade.php
+++ src/Facades/Facade.php
@@ -3,18 +3,16 @@
 namespace Floky\Facades;
 
 use Floky\Application;
-use Floky\Validation\Validator;
 
 class Facade
 {
 
-    protected static $class;
 
     public static function __callStatic($method, $args) {
 
         $services = Application::getInstance()->services();
 
-        return  call_user_func_array([$services->get(self::$class), $method], $args);
+        return  call_user_func_array([$services->get(static::getTargetClass()), $method], $args);
 
     } 
 }
--- src/Facades/Validator.php
+++ src/Facades/Validator.php
@@ -2,19 +2,16 @@
 
 namespace Floky\Facades;
 
-use Floky\Validation\Validator as ValidationValidator;
-
 /**
  * @method static mixed validate(array $data, array $rules, array $messages = [])
  */
 class Validator extends Facade
 {
 
-    protected static $class = ValidationValidator::class;
 
     protected static function getTargetClass(): string
     {
      
-        return ValidationValidator::class;
+        return \Floky\Validation\Validator::class;
     }
 }
